1af279dacdd28a7a0dda79c9b56f4cb4e4a60200
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-10-01 22:25:12 +0530

    Receiving accelerometer updates

--- XYZ-machine/ViewController.swift
+++ XYZ-machine/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
@@ -14,11 +15,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var yLabel: UILabel!
     @IBOutlet weak var zLabel: UILabel!
     
+    var motionManager:  CMMotionManager!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates(to: .main, withHandler: updateLabel)
         
     }
 
+    func updateLabel(data: CMAccelerometerData?, error: Error?) {
+        guard let accelerometerData = data else { return }
+        print(accelerometerData)
+    }
 
 }
 
